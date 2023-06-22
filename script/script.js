295b1d16fc18f0e4674c01f1e59abfeab8f422e7
anbaaa <87261628+anbaaa@users.noreply.github.com>
2023-06-22 18:30:19 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+//메뉴
+$(".navi li").mouseover(function() {
+	$(".submenu").stop ().slideDown();
+	});
+
+$(".navi li").mouseleave(function () {
+	$(".submenu").stop().slideUp();
+	});	    
+	
+	
+//이미지슬라이딩
+ var imgs=2;
+  var now=0;
+
+  start( );
+
+  function start(){
+  $(".imgslide>a").eq(0).siblings( ).animate({width:"-1250px"});
+  
+  setInterval(function(){
+  now = now==imgs?0:now+=1;
+  $(".imgslide>a").eq(now-1).animate({width:"-1250px"});   
+  $(".imgslide>a").eq(now).animate({width:"1250px"});},3000); 
+  }
+  
+ // 모달
+ $(".notice1").click(function( ) {
+	 $(".modal").show( );
+	 });
+ $(".button button").click(function( ) {
+	 $(".modal").hide( ); 
+ 	});	 
